cf968becc4e7ea028d9a7917ba0492c26619f5b3
Karthik <karthiknair2004@gmail.com>
2025-05-23 01:21:59 +0530

    Auth , Profile API's

---
+++ src/routes/auth.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const authRouter = express.Router();
+const { validateSignUpData } = require("../utils/validation");
+const User = require("../models/user"); 
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken"); 
+
+authRouter.post("/signup", async (req, res) => {
+  try {
+    // Validation of data
+    validateSignUpData(req);
+
+    const { firstName, lastName, emailId, password } = req.body;
+
+    // Encrypting the password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // Creating a new instance of the User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
+    await user.save();
+    res.send("User Added Successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid Credentials");
+    }
+
+    const isPasswordValid = await user.validatePassword(password);
+
+    if (isPasswordValid) {
+      // Create a JWT token
+      const token = await user.getJWT();
+      console.log(token);
+
+      res.cookie("token", token, {
+        expires : new Date(Date.now() + 8 * 3600000),
+      });
+      res.send("Login Successful");
+    } else {
+      throw new Error("Password is not correct");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR :" + err.message);
+  }
+});
+
+authRouter.post("/logout", async (req, res) => {
+    res.cookie("token", null , {
+        expires : new Date(Date.now()),
+    });
+    res.send();
+});
+
+
+module.exports = authRouter;
